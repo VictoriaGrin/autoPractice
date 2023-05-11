ed28a065366adb03459f98a162329a1506771e3d
VictoriaGrin <vikagrin25@gmail.com>
2023-05-11 14:49:03 +0300

    the third commit

--- e2e-ui-tests/specs/signUp.spec.js
+++ e2e-ui-tests/specs/signUp.spec.js
@@ -17,8 +17,8 @@ test.describe('SingUp tests', () => {
 
     });
 
-    test.only('Registration with valid data', async ({ page }) => {
+    test('Registration with valid data', async ({ page }) => {
         await singUpPage.registration('qwertyy','qwer@hfghg.com','test_123');
-        await expect(page).toHaveURL('http://omega-stage.qa.nolimit.school/sign-in');
+        await expect.toHaveURL('http://omega-stage.qa.nolimit.school/sign-in');
     });
 })
